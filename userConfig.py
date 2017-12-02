a9321eb50e2710b631efa42b644e9c82e16b44b9
Bin Zhang <binzhang@MyMac.local>
2017-12-02 21:53:51 +0800

    add doctor_lists

---
+++ userConfig.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+setting = {}
+
+#邮箱用户名
+setting["mail_user"] = "akuma_cool@163.com"
+#邮箱密码
+setting["mail_pass"] = "Akuma7v8yps"
+#预约日期
+setting["reg_date"] = ["2017-12-08"]
+#
+setting["doctors_list"] = {'hospitalId': '10012', 'deptId': '12436', 'expertId': '12191'},\
+                          {'hospitalId': '10012', 'deptId': '12436', 'expertId': '12374'},\
+                          {'hospitalId': '10012', 'deptId': '12436', 'expertId': '12427'},\
+                          {'hospitalId': '10012', 'deptId': '12436', 'expertId': '12064'},\
+                          {'hospitalId': '10012', 'deptId': '12436', 'expertId': '12063'}
